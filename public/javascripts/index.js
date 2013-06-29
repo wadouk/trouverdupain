f18ea93d7d5f549767396691870bc419746df8a7
Nicolas Bétheuil <wadouk@gmail.com>
2013-06-29 18:09:09 +0200

    display if bakery should be opened or not

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,21 @@
 function initialize() {
 
     function ajax(p) {
-        console.log('ajax', p);
         var r = new XMLHttpRequest();
         r.onreadystatechange = function () {
-            console.log('ajax d', r);
             if (r.readyState != 4 || r.status != 200) return;
             p.success(r);
         };
         r.open(p.verb || 'GET', p.url, true);
         r.send();
     }
+
+    var bakeryIconOpen = L.icon({
+        iconUrl: 'images/pitr_bakery_croissant.png'
+    });
+    var bakeryIconClose = L.icon({
+        iconUrl: 'images/pitr_bakery_croissant_close.png'
+    });
 
     function displayMarkers(r) {
         function onEachFeature(feature, layer) {
@@ -27,13 +32,40 @@ function initialize() {
             }
         }
 
-        function conge(num) {
+        function numJourFermeture(fermeture) {
+            return [
+                "Dimanche",
+                "Lundi",
+                "Mardi",
+                "Mercredi",
+                "Jeudi",
+                "Vendredi",
+                "Samedi"
+            ].indexOf(fermeture);
+        }
+
+        function isClose(properties) {
+            var numJourFermeture2 = numJourFermeture(properties.fermeture);
+            var congeMonth2 = congeMonth(properties.conge);
+            var b = (numJourFermeture2 === new Date().getDay() || congeMonth2 === new Date().getMonth());
+            return ( b)
+        }
+
+
+        function congeMonth(num) {
             //2007 Juillet Aout
             //1      x
             //2             x
             var fullYear = new Date().getFullYear();
             var congeAout2007 = (fullYear - 2007) % 2 == 0;
-            return (num == 2 && congeAout2007 ? "en Aout" : "en Juillet") + " (Groupe " + num + ")";
+            return (num == 2 && congeAout2007 ? 8 : 7);
+        }
+
+        function conge(num) {
+            //2007 Juillet Aout
+            //1      x
+            //2             x
+            return (congeMonth(num) === 8 ? "en Aout" : "en Juillet") + " (Groupe " + num + ")";
         }
 
         var points = JSON.parse(r.responseText);
@@ -50,29 +82,31 @@ function initialize() {
                     type: "Point",
                     coordinates: p.coordinates
                 },
-                properties: { popupContent: popupContent }
+                properties: { popupContent: popupContent,
+                    fermeture: p.properties.fermeture,
+                    conge: p.properties.conge
+                }
             };
         });
-        console.log(geojson);
         L.geoJson({
             features: geojson
         }, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {icon: isClose(feature.properties) ? bakeryIconClose : bakeryIconOpen});
+            },
             onEachFeature: onEachFeature
         }).addTo(map);
     }
 
     function onLocationFound(e) {
-        console.log('onlocation', e);
         L.circle(e.latlng, e.accuracy / 2).addTo(map);
         ajax({
             url: '/near', success: displayMarkers
         });
     }
 
-
-    console.log("init");
     var map = L.map('map');
-    L.tileLayer('http://{s}.tile.cloudmade.com/553fcdd9327a493e847991b2074535c0/997/256/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.cloudmade.com/553fcdd9327a493e847991b2074535c0/100985/256/{z}/{x}/{y}.png', {
         maxZoom: 18
     }).addTo(map);
     map.locate({setView: true, maxZoom: 16});
